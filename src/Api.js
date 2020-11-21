a7670d6e7558a53b158ffe5d96337154acb18042
Andrew Kim <jeesoo2002@gmail.com>
2020-11-21 19:38:27 +0900

    img background with styled comp done, layout with flexbox done

--- src/Api.js
+++ src/Api.js
@@ -1,17 +1,23 @@
 import react, {useState, useEffect} from 'react';
 import Card from './Card';
+import './Api.css';
 
 const Articles = () => {
   const [articles, setArticles] = useState({});
   const [hasError, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostsPerPage] = useState(10);
   const NYT_API_KEY = process.env.REACT_APP_NYT_API_KEY;
 
   async function fetchData() {
+    setLoading(true);
     const res = await fetch(`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=${NYT_API_KEY}`)
     res
       .json()
       .then(res => {
         setArticles(res.response.docs);
+        setLoading(false);
       })
       .catch(err => setError(err));
   }
@@ -21,9 +27,9 @@ const Articles = () => {
   }, [])
 
   return (
-    <div>
-      {articles.length && articles.map((article) => {
-        return <Card article={article} />
+    <div className="CardWrapper">
+      {articles.length && articles.map((article, i) => {
+        return <Card key={i} article={article} />
       })}
       <hr />
       <span>Has error: {JSON.stringify(hasError)}</span>
--- src/Card.js
+++ src/Card.js
@@ -1,15 +1,32 @@
 import './Card.css';
+import styled from 'styled-components';
+
+const Image = styled.div`
+  background-image: url(${props => props.backgroundImage});
+  min-height: 200px;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  border-radius: 9px 9px 0 0;
+`;
 
 const Card = ({article}) => {
+  // console.log(article);
   const content = article.lead_paragraph;
-  console.log(article);
   const content_url = article.web_url;
+  const img_url_prefix = process.env.REACT_APP_NYT_IMG_PREFIX;
+  const img_placeholder = "https://via.placeholder.com/150x200?text=Tester";
+  const img_url = article.multimedia.length > 0 ? img_url_prefix + article.multimedia[0].url : img_placeholder;
+  console.log(article.multimedia.length);
+  console.log(img_url);
+
   return (
-    <a href={content_url}>
       <div className="Card">
-        <p>{content.length > 30 ? content.substr(0, 29) + `...more` : content}</p>
+        <Image backgroundImage={img_url} />
+        <a href={content_url} className="CardContentWrapper">
+          <p>{content.length > 30 ? content.substr(0, 29) + `...more` : content}</p>
+        </a>
       </div>
-    </a>
   )
 }
 
